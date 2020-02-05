d1c32c0aaa79419da438ae8bb40b300c433ede88
Dan Funk <daniel.h.funk@gmail.com>
2020-02-05 15:17:47 -0500

    Travis configuration for connecting to postgres.

---
+++ config/travis-testing.py
@@ -0,0 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+NAME = "CR Connect Workflow"
+CORS_ENABLED = False
+DEVELOPMENT = True
+TESTING = True
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:@localhost:5432/crc_test"
