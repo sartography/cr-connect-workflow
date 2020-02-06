42ce7aaf0755946a0a72ed9cd8151126a6182afd
Dan Funk <daniel.h.funk@gmail.com>
2020-02-06 09:45:24 -0500

    Fixing more stupid mistakes in the docker file.

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,5 @@
 # run migrations
-ENV FLASK_APP=./crc/__init__.py
-RUN pipenv run flask db upgrade
-RUN pipenv run flask load-example-data
+export FLASK_APP=./crc/__init__.py
+pipenv run flask db upgradeo
+pipenv run flask load-example-data
 pipenv run python ./run.py
