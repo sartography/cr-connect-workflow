305118e90ee4ec7e73330b3a8d82f05c6ef67e96
Aaron Louie <ajlouie@gmail.com>
2020-03-02 14:42:30 -0500

    Adds a test for get_studies endpoint

--- tests/test_study_api.py
+++ tests/test_study_api.py
@@ -1,11 +1,13 @@
 import json
 from datetime import datetime, timezone
+from unittest.mock import patch
 
 from crc import session
 from crc.models.study import StudyModel, StudyModelSchema
 from crc.models.protocol_builder import ProtocolBuilderStatus
 from crc.models.workflow import WorkflowSpecModel, WorkflowSpecModelSchema, WorkflowModel, WorkflowStatus, \
     WorkflowApiSchema
+from services.protocol_builder import ProtocolBuilderService
 from tests.base_test import BaseTest
 
 
@@ -58,6 +60,44 @@ class TestStudyApi(BaseTest):
         self.assertIsNotNone(db_study)
         self.assertEqual(study.title, db_study.title)
         self.assertEqual(study.protocol_builder_status, db_study.protocol_builder_status)
+
+
+    @patch('crc.services.protocol_builder.requests.get')
+    def test_get_all_studies(self, mock_get):
+        self.load_example_data()
+        db_studies_before = session.query(StudyModel).all()
+        num_db_studies_before = len(db_studies_before)
+
+        mock_get.return_value.ok = True
+        mock_get.return_value.text = self.protocol_builder_response('user_studies.json')
+        # pb_response = ProtocolBuilderService.get_studies(self.test_uid)
+        # self.assertIsNotNone(pb_response)
+
+        self.load_example_data()
+        api_response = self.app.get('/v1.0/study',
+                          follow_redirects=True,
+                          headers=self.logged_in_headers(),
+                          content_type="application/json")
+        self.assert_success(api_response)
+
+        db_studies_after = session.query(StudyModel).all()
+        num_db_studies_after = len(db_studies_after)
+        self.assertGreater(num_db_studies_after, num_db_studies_before)
+
+        json_data = json.loads(api_response.get_data(as_text=True))
+        api_studies = StudyModelSchema(many=True).load(json_data, session=session)
+
+        num_inactive = 0
+        num_active = 0
+
+        for study in api_studies:
+            if study.inactive:
+                num_inactive += 1
+            else:
+                num_active += 1
+
+        self.assertEqual(num_inactive, num_db_studies_before)
+        self.assertEqual(num_active, num_db_studies_after - num_db_studies_before)
 
     def test_study_api_get_single_study(self):
         self.load_example_data()
