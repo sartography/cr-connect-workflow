38f9a70511c96d61e28d0c7f2e509668d99b2f83
Aaron Louie <ajlouie@gmail.com>
2020-04-15 13:25:12 -0400

    Converts dev flag to boolean

--- config/default.py
+++ config/default.py
@@ -5,14 +5,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 NAME = "CR Connect Workflow"
 CORS_ENABLED = False
-DEVELOPMENT = environ.get('DEVELOPMENT', default="True")
+DEVELOPMENT = bool(eval(environ.get('DEVELOPMENT', default="True")))
 
 DB_HOST = environ.get('DB_HOST', default="localhost")
 DB_PORT = environ.get('DB_PORT', default="5432")
 DB_NAME = environ.get('DB_NAME', default="crc_dev")
 DB_USER = environ.get('DB_USER', default="crc_user")
 DB_PASSWORD = environ.get('DB_PASSWORD', default="crc_pass")
-SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI', default="postgresql://%s:%s@%s:%s/%s" % (DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME))
+SQLALCHEMY_DATABASE_URI = environ.get(
+    'SQLALCHEMY_DATABASE_URI',
+    default="postgresql://%s:%s@%s:%s/%s" % (DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+)
 TOKEN_AUTH_TTL_HOURS = 2
 TOKEN_AUTH_SECRET_KEY = environ.get('TOKEN_AUTH_SECRET_KEY', default="Shhhh!!! This is secret!  And better darn well not show up in prod.")
 FRONTEND_AUTH_CALLBACK = environ.get('FRONTEND_AUTH_CALLBACK', default="http://localhost:4200/session")
