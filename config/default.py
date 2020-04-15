4428dfe819febd2669e47d0ba0c8c70fa3442d89
Aaron Louie <ajlouie@gmail.com>
2020-04-15 13:36:39 -0400

    Removes eval. Just casting string to bool works.

--- config/default.py
+++ config/default.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 NAME = "CR Connect Workflow"
 CORS_ENABLED = False
-DEVELOPMENT = bool(eval(environ.get('DEVELOPMENT', default="True")))
+DEVELOPMENT = bool(environ.get('DEVELOPMENT', default="True"))
 
 DB_HOST = environ.get('DB_HOST', default="localhost")
 DB_PORT = environ.get('DB_PORT', default="5432")
