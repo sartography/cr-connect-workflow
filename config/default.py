b4fdba0fc9581cfad31d5a20bbcd301e3ba0fd19
Aaron Louie <ajlouie@gmail.com>
2020-04-15 17:22:41 -0400

    Sets production environment variable

--- config/default.py
+++ config/default.py
@@ -7,6 +7,7 @@ NAME = "CR Connect Workflow"
 CORS_ENABLED = False
 DEVELOPMENT = environ.get('DEVELOPMENT', default="true") == "true"
 TESTING = environ.get('TESTING', default="false") == "true"
+PRODUCTION = (environ.get('PRODUCTION', default="false") == "true") or (not DEVELOPMENT and not TESTING)
 
 DB_HOST = environ.get('DB_HOST', default="localhost")
 DB_PORT = environ.get('DB_PORT', default="5432")
