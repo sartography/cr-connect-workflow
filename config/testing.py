dffba7dc9224c27fc8da888ff66e6c1915569432
Aaron Louie <ajlouie@gmail.com>
2020-04-15 14:14:58 -0400

    Prints some more config settings for debugging

--- config/testing.py
+++ config/testing.py
@@ -7,3 +7,8 @@ DEVELOPMENT = True
 TESTING = True
 SQLALCHEMY_DATABASE_URI = "postgresql://crc_user:crc_pass@localhost:5432/crc_test"
 TOKEN_AUTH_SECRET_KEY = "Shhhh!!! This is secret!  And better darn well not show up in prod."
+
+print('### USING TESTING CONFIG: ###')
+print('SQLALCHEMY_DATABASE_URI = ', SQLALCHEMY_DATABASE_URI)
+print('DEVELOPMENT = ', DEVELOPMENT)
+print('TESTING = ', TESTING)
