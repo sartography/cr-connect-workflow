14c82f4044fa214684b47c14212483804198cb2c
Aaron Louie <ajlouie@gmail.com>
2020-04-23 14:47:07 -0400

    Adds a 30 second delay before refreshing DC/OS, in case Docker Hub takes a bit to update its cache

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,10 @@ echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build --no-cache -t sartography/cr-connect-backend:latest . || exit 1;
 docker push sartography/cr-connect-backend:latest || exit 1;
 
+# Wait for Docker Hub
+echo "Publishing to Docker Hub..."
+sleep 30
+
 # Notify UVA DCOS that Docker image has been updated
+echo "Refreshing DC/OS..."
 aws sqs send-message --queue-url 'https://queue.amazonaws.com/474683445819/dcos-refresh' --message-body 'crconnect/backend' || exit 1;
