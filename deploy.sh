559494aee238f46204f98aea7b88603605534462
Aaron Louie <ajlouie@gmail.com>
2020-05-07 10:47:24 -0400

    Fails build if any docker or aws command errors out

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Build and push Docker image to Docker Hub
-echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin || exit 1
 REPO="sartography/cr-connect-workflow"
 TAG=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo "$TRAVIS_BRANCH" ; fi)
-docker build -f Dockerfile -t "$REPO:$COMMIT" .
-docker tag "$REPO:$COMMIT" "$REPO:$TAG"
-docker tag "$REPO:$COMMIT" "$REPO:travis-$TRAVIS_BUILD_NUMBER"
-docker push "$REPO"
+docker build -f Dockerfile -t "$REPO:$COMMIT" . || exit 1
+docker tag "$REPO:$COMMIT" "$REPO:$TAG" || exit 1
+docker tag "$REPO:$COMMIT" "$REPO:travis-$TRAVIS_BUILD_NUMBER" || exit 1
+docker push "$REPO" || exit 1
 
 # Wait for Docker Hub
 echo "Publishing to Docker Hub..."
@@ -16,4 +16,4 @@ sleep 30
 # Notify DC/OS that Docker image has been updated
 echo "Refreshing DC/OS..."
 STAGE=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "production"; else echo "$TRAVIS_BRANCH" ; fi)
-aws sqs send-message --region "$AWS_DEFAULT_REGION" --queue-url "$AWS_SQS_URL" --message-body "crconnect/$STAGE/backend"
+aws sqs send-message --region "$AWS_DEFAULT_REGION" --queue-url "$AWS_SQS_URL" --message-body "crconnect/$STAGE/backend" || exit 1
