9af91bed8b8d4fd1096fb400327dcef69e3097f6
Aaron Louie <ajlouie@gmail.com>
2020-05-07 13:49:17 -0400

    Printing out some stuff for debugging

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,14 @@
 echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin || exit 1
 REPO="sartography/cr-connect-workflow"
 TAG=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo "$TRAVIS_BRANCH" ; fi)
+
+echo "REPO = $REPO"
+echo "TAG = $TAG"
+echo "COMMIT = $COMMIT"
+echo "REPO:COMMIT = $REPO:$COMMIT"
+echo "REPO:TAG = $REPO:$TAG"
+echo "REPO:travis-TRAVIS_BUILD_NUMBER = $REPO:travis-$TRAVIS_BUILD_NUMBER"
+
 docker build -f Dockerfile -t "$REPO:$COMMIT" . || exit 1
 docker tag "$REPO:$COMMIT" "$REPO:$TAG" || exit 1
 docker tag "$REPO:$COMMIT" "$REPO:travis-$TRAVIS_BUILD_NUMBER" || exit 1
@@ -16,4 +24,5 @@ sleep 30
 # Notify DC/OS that Docker image has been updated
 echo "Refreshing DC/OS..."
 STAGE=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "production"; else echo "$TRAVIS_BRANCH" ; fi)
+echo "STAGE = $STAGE"
 aws sqs send-message --region "$AWS_DEFAULT_REGION" --queue-url "$AWS_SQS_URL" --message-body "crconnect/$STAGE/backend" || exit 1
