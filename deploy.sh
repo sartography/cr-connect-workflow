a1ad11c07d683cd48e50b92e7f9e9017e3fb930f
Aaron Louie <ajlouie@gmail.com>
2020-05-07 14:27:32 -0400

    Cleans up debugging stuff

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,6 @@ REPO="sartography/cr-connect-workflow"
 TAG=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo "$TRAVIS_BRANCH" ; fi)
 COMMIT=${TRAVIS_COMMIT::8}
 
-echo "REPO = $REPO"
-echo "TAG = $TAG"
-echo "TRAVIS_COMMIT = $TRAVIS_COMMIT"
-echo "COMMIT = $COMMIT"
-echo "REPO:COMMIT = $REPO:$COMMIT"
-echo "REPO:TAG = $REPO:$TAG"
-echo "REPO:travis-TRAVIS_BUILD_NUMBER = $REPO:travis-$TRAVIS_BUILD_NUMBER"
-
 docker build -f Dockerfile -t "$REPO:$COMMIT" . || exit 1
 docker tag "$REPO:$COMMIT" "$REPO:$TAG" || exit 1
 docker tag "$REPO:$COMMIT" "$REPO:travis-$TRAVIS_BUILD_NUMBER" || exit 1
