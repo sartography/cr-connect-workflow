1cc9a60bfe9ddef1509c9d24108d1d73d416b37d
Dan Funk <daniel.h.funk@gmail.com>
2020-05-22 08:01:33 -0400

    clearing out the remaining references to the flask_sso library.

--- crc/__init__.py
+++ crc/__init__.py
@@ -6,7 +6,6 @@ from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_sso import SSO
 
 logging.basicConfig(level=logging.INFO)
 
