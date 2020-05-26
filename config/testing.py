0792aa40a51e2b8fc530ace52658fcb440bf287f
Dan Funk <daniel.h.funk@gmail.com>
2020-05-25 22:28:33 -0400

    Can't override travis settings if you do it with the testing file.

--- config/testing.py
+++ config/testing.py
@@ -1,22 +1,10 @@
 import os
-from os import environ
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 NAME = "CR Connect Workflow"
 DEVELOPMENT = True
 TESTING = True
-
-DB_HOST = environ.get('DB_HOST', default="localhost")
-DB_PORT = environ.get('DB_PORT', default="5432")
-DB_NAME = environ.get('DB_NAME', default="crc_dev")
-DB_USER = environ.get('DB_USER', default="crc_user")
-DB_PASSWORD = environ.get('DB_PASSWORD', default="crc_pass")
-SQLALCHEMY_DATABASE_URI = environ.get(
-    'SQLALCHEMY_DATABASE_URI',
-    default="postgresql://%s:%s@%s:%s/%s" % (DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
-)
-
+SQLALCHEMY_DATABASE_URI = "postgresql://crc_user:crc_pass@localhost:5432/crc_test"
 TOKEN_AUTH_SECRET_KEY = "Shhhh!!! This is secret!  And better darn well not show up in prod."
 PB_ENABLED = False
 
--- config/travis-testing.py
+++ config/travis-testing.py
@@ -1,28 +1,27 @@
 import os
+from os import environ
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 NAME = "CR Connect Workflow"
 DEVELOPMENT = True
 TESTING = True
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:@localhost:5432/crc_test"
 TOKEN_AUTH_TTL_HOURS = 2
 TOKEN_AUTH_SECRET_KEY = "Shhhh!!! This is secret!  And better darn well not show up in prod."
 FRONTEND_AUTH_CALLBACK = "http://localhost:4200/session"  # Not Required
 
-#: Default attribute map for single signon.
-SSO_ATTRIBUTE_MAP = {
-    'eppn': (False, 'eppn'),  # dhf8r@virginia.edu
-    'uid': (True, 'uid'),  # dhf8r
-    'givenName': (False, 'first_name'),  # Daniel
-    'mail': (False, 'email_address'),  # dhf8r@Virginia.EDU
-    'sn': (False, 'last_name'),  # Funk
-    'affiliation': (False, 'affiliation'),  # 'staff@virginia.edu;member@virginia.edu'
-    'displayName': (False, 'display_name'),  # Daniel Harold Funk
-    'title': (False, 'title')  # SOFTWARE ENGINEER V
-}
+DB_HOST = environ.get('DB_HOST', default="localhost")
+DB_PORT = environ.get('DB_PORT', default="5432")
+DB_NAME = environ.get('DB_NAME', default="crc_dev")
+DB_USER = environ.get('DB_USER', default="crc_user")
+DB_PASSWORD = environ.get('DB_PASSWORD', default="crc_pass")
+SQLALCHEMY_DATABASE_URI = environ.get(
+    'SQLALCHEMY_DATABASE_URI',
+    default="postgresql://%s:%s@%s:%s/%s" % (DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+)
 
 print('+++ USING TRAVIS TESTING CONFIG: +++')
-print('SQLALCHEMY_DATABASE_URI = ', SQLALCHEMY_DATABASE_URI)
 print('DEVELOPMENT = ', DEVELOPMENT)
 print('TESTING = ', TESTING)
 print('FRONTEND_AUTH_CALLBACK = ', FRONTEND_AUTH_CALLBACK)
+print('DB_HOST = ', DB_HOST)
