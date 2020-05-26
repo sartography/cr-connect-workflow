cbaedad0f63ef66877205126395a26b2bdebdb65
Dan Funk <daniel.h.funk@gmail.com>
2020-05-25 22:02:06 -0400

    have testing respect the environment variables if they are set, so we can set them in travis, and use the testing environment there.

--- config/testing.py
+++ config/testing.py
@@ -1,10 +1,22 @@
 import os
+from os import environ
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 NAME = "CR Connect Workflow"
 DEVELOPMENT = True
 TESTING = True
-SQLALCHEMY_DATABASE_URI = "postgresql://crc_user:crc_pass@localhost:5432/crc_test"
+
+DB_HOST = environ.get('DB_HOST', default="localhost")
+DB_PORT = environ.get('DB_PORT', default="5432")
+DB_NAME = environ.get('DB_NAME', default="crc_dev")
+DB_USER = environ.get('DB_USER', default="crc_user")
+DB_PASSWORD = environ.get('DB_PASSWORD', default="crc_pass")
+SQLALCHEMY_DATABASE_URI = environ.get(
+    'SQLALCHEMY_DATABASE_URI',
+    default="postgresql://%s:%s@%s:%s/%s" % (DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+)
+
 TOKEN_AUTH_SECRET_KEY = "Shhhh!!! This is secret!  And better darn well not show up in prod."
 PB_ENABLED = False
 
