040a95281e7bceb6c3f5e6ed5032bc5de31a7d3c
Aaron Louie <ajlouie@gmail.com>
2020-07-15 17:09:34 -0400

    Updates package hashes

--- tests/workflow/test_workflow_service.py
+++ tests/workflow/test_workflow_service.py
@@ -132,7 +132,6 @@ class TestWorkflowService(BaseTest):
         # The first task should be empty, with all the data removed.
         self.assertEqual({}, task_logs[0].form_data)
 
-
     def test_dmn_evaluation_errors_in_oncomplete_raise_api_errors_during_validation(self):
         workflow_spec_model = self.load_test_spec("decision_table_invalid")
         with self.assertRaises(ApiError):
