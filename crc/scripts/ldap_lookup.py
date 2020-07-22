522f848682f296351ec8d849ab5ee07d37e70df6
Carlos Lopez <carl.lopez@gmail.com>
2020-07-21 20:53:24 -0600

    Addressing feedback

--- crc/scripts/ldap_lookup.py
+++ crc/scripts/ldap_lookup.py
@@ -6,73 +6,55 @@ from crc.scripts.script import Script
 from crc.services.ldap_service import LdapService
 
 
-USER_DETAILS = {
-    "PIComputingID": {
-        "value": "",
-        "data": {
-         },
-        "label": "invalid uid"
-    }
-}
-
-
-class LdapLookup(Script):
+class LdapReplace(Script):
     """This Script allows to be introduced as part of a workflow and called from there, taking
-    a UID as input and looking it up through LDAP to return the person's details """
+    a UID (or several) as input and looking it up through LDAP to return the person's details """
 
     def get_description(self):
         return """
 Attempts to create a dictionary with person details, using the
 provided argument (a UID) and look it up through LDAP.
 
-Example:
-LdapLookup PIComputingID
+Examples:
+#! LdapReplace supervisor
+#! LdapReplace supervisor collaborator
+#! LdapReplace supervisor cosupervisor collaborator
 """
 
     def do_task_validate_only(self, task, *args, **kwargs):
-        self.get_user_info(task, args)
+        self.set_users_info_in_task(task, args)
 
     def do_task(self, task, *args, **kwargs):
         args = [arg for arg in args if type(arg) == str]
-        user_info = self.get_user_info(task, args)
+        self.set_users_info_in_task(task, args)
 
-        user_details = copy.deepcopy(USER_DETAILS)
-        user_details['PIComputingID']['value'] = user_info['uid']
-        if len(user_info.keys()) > 1:
-            user_details['PIComputingID']['label'] = user_info.pop('label')
-        else:
-            user_info.pop('uid')
-        user_details['PIComputingID']['data'] = user_info
-        return user_details
-
-    def get_user_info(self, task, args):
+    def set_users_info_in_task(self, task, args):
         if len(args) < 1:
             raise ApiError(code="missing_argument",
-                           message="Ldap lookup script requires one argument. The "
-                                   "UID for the person we want to look up")
+                           message="Ldap replace script requires at least one argument. The "
+                                   "UID for the person(s) we want to look up")
 
-        arg = args.pop()  # Extracting only one value for now
-        uid = task.workflow.script_engine.evaluate_expression(task, arg)
-        if not isinstance(uid, str):
-            raise ApiError(code="invalid_argument",
-                           message="Ldap lookup script requires one 1 UID argument, of type string.")
-        user_info_dict = {}
-        try:
-            user_info = LdapService.user_info(uid)
-            user_info_dict = {
-                "display_name": user_info.display_name,
-                "given_name": user_info.given_name,
-                "email_address": user_info.email_address,
-                "telephone_number": user_info.telephone_number,
-                "title": user_info.title,
-                "department": user_info.department,
-                "affiliation": user_info.affiliation,
-                "sponsor_type": user_info.sponsor_type,
-                "uid": user_info.uid,
-                "label": user_info.proper_name()
-            }
-        except:
-            user_info_dict['uid'] = uid
-            app.logger.error(f'Ldap lookup failed for UID {uid}')
-
-        return user_info_dict
+        users_info = {}
+        for arg in args:
+            uid = task.workflow.script_engine.evaluate_expression(task, arg)
+            if not isinstance(uid, str):
+                raise ApiError(code="invalid_argument",
+                               message="Ldap replace script found an invalid argument, type string is required")
+            user_info_dict = {}
+            try:
+                user_info = LdapService.user_info(uid)
+                user_info_dict = {
+                    "display_name": user_info.display_name,
+                    "given_name": user_info.given_name,
+                    "email_address": user_info.email_address,
+                    "telephone_number": user_info.telephone_number,
+                    "title": user_info.title,
+                    "department": user_info.department,
+                    "affiliation": user_info.affiliation,
+                    "sponsor_type": user_info.sponsor_type,
+                    "uid": user_info.uid,
+                    "proper_name": user_info.proper_name()
+                }
+            except:
+                app.logger.error(f'Ldap replace failed for UID {uid}')
+            task.data[arg] = user_info_dict
--- tests/ldap/test_ldap_lookup_script.py
+++ tests/ldap/test_ldap_lookup_script.py
@@ -1,7 +1,7 @@
 from tests.base_test import BaseTest
 
 from crc.services.workflow_processor import WorkflowProcessor
-from crc.scripts.ldap_lookup import LdapLookup
+from crc.scripts.ldap_lookup import LdapReplace
 from crc import db, mail
 
 
@@ -18,20 +18,56 @@ class TestLdapLookupScript(BaseTest):
           'PIComputingID': 'dhf8r'
         }
 
-        script = LdapLookup()
+        script = LdapReplace()
         user_details = script.do_task(task, workflow.study_id, workflow.id, "PIComputingID")
 
-        self.assertEqual(user_details['PIComputingID']['label'], 'Dan Funk - (dhf8r)')
-        self.assertEqual(user_details['PIComputingID']['value'], 'dhf8r')
-        self.assertEqual(user_details['PIComputingID']['data']['display_name'], 'Dan Funk')
-        self.assertEqual(user_details['PIComputingID']['data']['given_name'], 'Dan')
-        self.assertEqual(user_details['PIComputingID']['data']['email_address'], 'dhf8r@virginia.edu')
-        self.assertEqual(user_details['PIComputingID']['data']['telephone_number'], '+1 (434) 924-1723')
-        self.assertEqual(user_details['PIComputingID']['data']['title'], 'E42:He\'s a hoopy frood')
-        self.assertEqual(user_details['PIComputingID']['data']['department'], 'E0:EN-Eng Study of Parallel Universes')
-        self.assertEqual(user_details['PIComputingID']['data']['affiliation'], 'faculty')
-        self.assertEqual(user_details['PIComputingID']['data']['sponsor_type'], 'Staff')
-        self.assertEqual(user_details['PIComputingID']['data']['uid'], 'dhf8r')
+        self.assertEqual(task.data['PIComputingID']['display_name'], 'Dan Funk')
+        self.assertEqual(task.data['PIComputingID']['given_name'], 'Dan')
+        self.assertEqual(task.data['PIComputingID']['email_address'], 'dhf8r@virginia.edu')
+        self.assertEqual(task.data['PIComputingID']['telephone_number'], '+1 (434) 924-1723')
+        self.assertEqual(task.data['PIComputingID']['title'], 'E42:He\'s a hoopy frood')
+        self.assertEqual(task.data['PIComputingID']['department'], 'E0:EN-Eng Study of Parallel Universes')
+        self.assertEqual(task.data['PIComputingID']['affiliation'], 'faculty')
+        self.assertEqual(task.data['PIComputingID']['sponsor_type'], 'Staff')
+        self.assertEqual(task.data['PIComputingID']['uid'], 'dhf8r')
+        self.assertEqual(task.data['PIComputingID']['proper_name'], 'Dan Funk - (dhf8r)')
+
+    def test_get_existing_users_details(self):
+        self.load_example_data()
+        self.create_reference_document()
+        workflow = self.create_workflow('empty_workflow')
+        processor = WorkflowProcessor(workflow)
+        task = processor.next_task()
+
+        task.data = {
+          'supervisor': 'dhf8r',
+          'investigator': 'lb3dp'
+        }
+
+        script = LdapReplace()
+        user_details = script.do_task(task, workflow.study_id, workflow.id, "supervisor", "investigator")
+
+        self.assertEqual(task.data['supervisor']['display_name'], 'Dan Funk')
+        self.assertEqual(task.data['supervisor']['given_name'], 'Dan')
+        self.assertEqual(task.data['supervisor']['email_address'], 'dhf8r@virginia.edu')
+        self.assertEqual(task.data['supervisor']['telephone_number'], '+1 (434) 924-1723')
+        self.assertEqual(task.data['supervisor']['title'], 'E42:He\'s a hoopy frood')
+        self.assertEqual(task.data['supervisor']['department'], 'E0:EN-Eng Study of Parallel Universes')
+        self.assertEqual(task.data['supervisor']['affiliation'], 'faculty')
+        self.assertEqual(task.data['supervisor']['sponsor_type'], 'Staff')
+        self.assertEqual(task.data['supervisor']['uid'], 'dhf8r')
+        self.assertEqual(task.data['supervisor']['proper_name'], 'Dan Funk - (dhf8r)')
+
+        self.assertEqual(task.data['investigator']['display_name'], 'Laura Barnes')
+        self.assertEqual(task.data['investigator']['given_name'], 'Laura')
+        self.assertEqual(task.data['investigator']['email_address'], 'lb3dp@virginia.edu')
+        self.assertEqual(task.data['investigator']['telephone_number'], '+1 (434) 924-1723')
+        self.assertEqual(task.data['investigator']['title'], 'E0:Associate Professor of Systems and Information Engineering')
+        self.assertEqual(task.data['investigator']['department'], 'E0:EN-Eng Sys and Environment')
+        self.assertEqual(task.data['investigator']['affiliation'], 'faculty')
+        self.assertEqual(task.data['investigator']['sponsor_type'], 'Staff')
+        self.assertEqual(task.data['investigator']['uid'], 'lb3dp')
+        self.assertEqual(task.data['investigator']['proper_name'], 'Laura Barnes - (lb3dp)')
 
     def test_get_invalid_user_details(self):
         self.load_example_data()
@@ -44,8 +80,22 @@ class TestLdapLookupScript(BaseTest):
           'PIComputingID': 'rec3z'
         }
 
-        script = LdapLookup()
+        script = LdapReplace()
         user_details = script.do_task(task, workflow.study_id, workflow.id, "PIComputingID")
-        self.assertEqual(user_details['PIComputingID']['label'], 'invalid uid')
-        self.assertEqual(user_details['PIComputingID']['value'], 'rec3z')
-        self.assertEqual(user_details['PIComputingID']['data'], {})
+
+        self.assertEqual(task.data['PIComputingID'], {})
+
+    def test_bpmn_task_receives_user_details(self):
+        workflow = self.create_workflow('ldap_replace')
+
+        task_data = {
+          'Supervisor': 'dhf8r',
+          'Investigator': 'lb3dp'
+        }
+        task = self.get_workflow_api(workflow).next_task
+
+        self.complete_form(workflow, task, task_data)
+
+        task = self.get_workflow_api(workflow).next_task
+
+        self.assertEqual(task.data['Supervisor']['proper_name'], 'Dan Funk - (dhf8r)')
