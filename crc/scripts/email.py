67697b5076cb4f84f737ca1e5e36b91e82c87c6d
mike cullerton <michaelc@cullerton.com>
2021-03-11 11:32:21 -0500

    Lots of changes!
    Email Script:
    - The email script now accepts 3 keyword arguments; subject, recipients, and cc.
    - Subject and recipients are required.
    - Recipients (and cc) can be an email address or list of addresses.
    - You can use the string 'associated' in place of an email address, in which case we look up the users associated with a study that have send_email set to True.
    - Moved some helper methods to email_service.
    
    Email Service:
    - Added cc argument
    - Added helper methods previously in email script.
    - Modified get_rendered_content to accept message and data directly. Previously, we passed in the task and grabbed them from there. Now we can use the method outside a task.

--- crc/scripts/email.py
+++ crc/scripts/email.py
@@ -1,17 +1,10 @@
-import re
-
-import markdown
-from jinja2 import Template
-
 from crc import app
 from crc.api.common import ApiError
 from crc.models.user import UserModel
 from crc.scripts.script import Script
-from crc.services.ldap_service import LdapService
+# from crc.services.ldap_service import LdapService
 from crc.services.email_service import EmailService
 from crc.services.study_service import StudyService
-
-from flask import render_template, request
 
 
 class Email(Script):
@@ -20,84 +13,82 @@ class Email(Script):
 
     def get_description(self):
         return """
-Creates an email, using the provided arguments.  The first argument is the subject of the email, 
-all subsequent arguments should be email addresses in quotes, or variables containing an email address or a list
-of email addresses."
+Creates an email, using the provided `subject`, `recipients`, and `cc` arguments.  
+The recipients and cc arguments can contain an email address or list of email addresses. 
+In place of an email address, we accept the string 'associated', in which case we
+look up the users associated with the study who have send_email set to True. 
+The cc argument is not required.
 The "documentation" should contain markdown that will become the body of the email message.
-Example:
-email ("My Subject", "dhf8r@virginia.edu", pi.email)
+Examples:
+email (subject="My Subject", recipients=["dhf8r@virginia.edu", pi.email, 'associated'])
+email (subject="My Subject", recipients=["dhf8r@virginia.edu", pi.email], cc='associated')
 """
 
     def do_task_validate_only(self, task, study_id, workflow_id, *args, **kwargs):
         self.get_subject(kwargs['subject'])
-        self.get_email_recipients(kwargs['recipients'])
-        self.get_content(task)
+        self.get_email_addresses(kwargs['recipients'], study_id)
+        EmailService().get_rendered_content(task.task_spec.documentation, task.data)
 
     def do_task(self, task, study_id, workflow_id, *args, **kwargs):
 
         if 'subject' in kwargs and 'recipients' in kwargs:
             subject = self.get_subject(kwargs['subject'])
-
-            # we can send the email to all the people associated with a study
-            # who have send_email set to True
-            if kwargs['recipients'] == 'associated':
-                recipient_uids = []
-                recipients = []
-                associates = StudyService.get_study_associates(study_id)
-                for associate in associates:
-                    if associate['send_email'] is True:
-                        recipient_uids.append(associate['uid'])
-                        # Shoe.query.filter(Shoe.id.in_(my_list_of_ids)).all()
-                returned = UserModel.query.filter(UserModel.uid.in_(recipient_uids)).all()
-                for item in returned:
-                    recipients.append(item.email_address)
-            else:
-                recipients = self.get_email_recipients(kwargs['recipients'])
+            recipients = self.get_email_addresses(kwargs['recipients'], study_id)
+            cc = []
+            if 'cc' in kwargs and kwargs['cc'] is not None:
+                cc = self.get_email_addresses(kwargs['cc'], study_id)
 
         else:
             raise ApiError(code="missing_argument",
-                           message="Email script requires a subject and at least one email address as arguments")
+                           message="Email script requires a subject and at least one email recipient as arguments")
 
         if recipients:
-            content, content_html = self.get_content(task)
+            message = task.task_spec.documentation
+            data = task.data
+            content, content_html = EmailService().get_rendered_content(message, data)
             EmailService.add_email(
                 subject=subject,
                 sender=app.config['DEFAULT_SENDER'],
                 recipients=recipients,
                 content=content,
                 content_html=content_html,
+                cc=cc,
                 study_id=study_id
             )
 
-    @staticmethod
-    def check_valid_email(email):
-        # regex from https://emailregex.com/
-        regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-        if re.search(regex, email):
-            return True
-        else:
-            return False
-
-    def get_email_recipients(self, recipients):
+    def get_email_addresses(self, users, study_id):
         emails = []
+        emails_to_check = []
 
         # Recipient can be an email address or list of email addresses
-        if isinstance(recipients, str):
-            emails_to_check = [recipients]
-        elif isinstance(recipients, list):
-            emails_to_check = recipients
+        # We also accept the string 'associated', in which case we lookup
+        # all users associated with a study who have send_email set to True
+        if isinstance(users, str):
+            if users == 'associated':
+                associated_emails = self.get_associated_emails(study_id)
+                for email in associated_emails:
+                    emails_to_check.append(email)
+            else:
+                emails_to_check.append(users)
+        elif isinstance(users, list):
+            for user in users:
+                if user == 'associated':
+                    associated_emails = self.get_associated_emails(study_id)
+                    for email in associated_emails:
+                        emails_to_check.append(email)
+                else:
+                    emails_to_check.append(user)
         else:
             raise ApiError(code="invalid_argument",
-                           message=f"Email script requires a valid email address (or list of addresses), but received '{recipients}'")
+                           message=f"Email script requires a valid email address (or list of addresses), but we received '{users}'")
 
         for e in emails_to_check:
-            if self.check_valid_email(e):
+            if EmailService().check_valid_email(e):
                 emails.append(e)
             else:
                 raise ApiError(code="invalid_argument",
                                message="The email address you provided could not be parsed. "
                                        "The value you provided is '%s" % e)
-
         return emails
 
     @staticmethod
@@ -106,18 +97,17 @@ email ("My Subject", "dhf8r@virginia.edu", pi.email)
             raise ApiError(code="invalid_argument",
                            message="The subject you provided could not be parsed. "
                                "The value is \"%s\" " % subject)
-
         return subject
 
-    def get_content(self, task):
-        content = task.task_spec.documentation
-        template = Template(content)
-        rendered = template.render(task.data)
-        rendered_markdown = markdown.markdown(rendered)
-        wrapped = self.get_cr_connect_wrapper(rendered_markdown)
-
-        return rendered, wrapped
-
     @staticmethod
-    def get_cr_connect_wrapper(email_body):
-        return render_template('mail_content_template.html', email_body=email_body, base_url=request.base_url)
+    def get_associated_emails(study_id):
+        associated_uids = []
+        associated_emails = []
+        associates = StudyService.get_study_associates(study_id)
+        for associate in associates:
+            if associate['send_email'] is True:
+                associated_uids.append(associate['uid'])
+        returned = UserModel.query.filter(UserModel.uid.in_(associated_uids)).all()
+        for item in returned:
+            associated_emails.append(item.email_address)
+        return associated_emails
--- crc/services/email_service.py
+++ crc/services/email_service.py
@@ -1,5 +1,10 @@
+import markdown
+import re
+
 from datetime import datetime
+from flask import render_template, request
 from flask_mail import Message
+from jinja2 import Template
 from sqlalchemy import desc
 
 from crc import app, db, mail, session
@@ -9,11 +14,12 @@ from crc.models.study import StudyModel
 from crc.models.email import EmailModel
 
 
+
 class EmailService(object):
     """Provides common tools for working with an Email"""
 
     @staticmethod
-    def add_email(subject, sender, recipients, content, content_html, study_id=None):
+    def add_email(subject, sender, recipients, content, content_html, cc=None, study_id=None):
         """We will receive all data related to an email and store it"""
 
         # Find corresponding study - if any
@@ -28,11 +34,12 @@ class EmailService(object):
         # Send mail
         try:
             msg = Message(subject,
-                  sender=sender,
-                  recipients=recipients)
+                          sender=sender,
+                          recipients=recipients)
 
             msg.body = content
             msg.html = content_html
+            msg.cc = cc
 
             mail.send(msg)
         except Exception as e:
@@ -41,3 +48,24 @@ class EmailService(object):
 
         db.session.add(email_model)
         db.session.commit()
+
+    @staticmethod
+    def check_valid_email(email):
+        # regex from https://emailregex.com/
+        regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        if re.search(regex, email):
+            return True
+        else:
+            return False
+
+    def get_rendered_content(self, message, data):
+        template = Template(message)
+        rendered = template.render(data)
+        rendered_markdown = markdown.markdown(rendered)
+        wrapped = self.get_cr_connect_wrapper(rendered_markdown)
+
+        return rendered, wrapped
+
+    @staticmethod
+    def get_cr_connect_wrapper(email_body):
+        return render_template('mail_content_template.html', email_body=email_body, base_url=request.base_url)
