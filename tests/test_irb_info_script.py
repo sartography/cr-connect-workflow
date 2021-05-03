c8446bfafd5e66e58da65f5f250e3c67ac7400f6
mike cullerton <michaelc@cullerton.com>
2021-05-03 14:51:27 -0400

    Mock the Protocol Builder response in my test

--- tests/test_irb_info_script.py
+++ tests/test_irb_info_script.py
@@ -1,13 +1,18 @@
 from tests.base_test import BaseTest
-from crc.services.protocol_builder import ProtocolBuilderService
 
 from crc import app, session
+from crc.services.protocol_builder import ProtocolBuilderService
+
+from unittest.mock import patch
 
 
 class TestIRBInfo(BaseTest):
 
-    def test_irb_info_script(self):
+    @patch('crc.services.protocol_builder.requests.get')
+    def test_irb_info_script(self, mock_get):
         app.config['PB_ENABLED'] = True
+        mock_get.return_value.ok = True
+        mock_get.return_value.text = self.protocol_builder_response('irb_info.json')
         workflow = self.create_workflow('irb_info_script')
         irb_info = ProtocolBuilderService.get_irb_info(workflow.study_id)
         workflow_api = self.get_workflow_api(workflow)
