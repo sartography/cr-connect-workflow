375089e5dfcff6f1618ba8c8ddbfcecb0afe600a
mike cullerton <michaelc@cullerton.com>
2021-05-06 15:32:28 -0400

    test and bpmn file for new get_dashboard_url script

--- tests/test_get_dashboard_url_script.py
+++ tests/test_get_dashboard_url_script.py
@@ -1,7 +1,17 @@
 from tests.base_test import BaseTest
+from crc import app, mail
 
 
 class TestGetDashboardURL(BaseTest):
 
     def test_get_dashboard_url(self):
-        pass
+        with mail.record_messages() as outbox:
+
+            dashboard_url = f'http://{app.config["FRONTEND"]}'
+            workflow = self.create_workflow('email_dashboard_url')
+            self.get_workflow_api(workflow)
+
+            self.assertEqual(1, len(outbox))
+            self.assertEqual('My Email Subject', outbox[0].subject)
+            self.assertEqual(['test@example.com'], outbox[0].recipients)
+            self.assertIn(dashboard_url, outbox[0].body)
