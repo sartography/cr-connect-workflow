4dc4e270aa0163cc4fb893f4ac6c6c001ba896a2
mike cullerton <michaelc@cullerton.com>
2021-05-06 15:54:39 -0400

    Fixed the test to also use HTTPS

--- tests/test_get_dashboard_url_script.py
+++ tests/test_get_dashboard_url_script.py
@@ -7,7 +7,7 @@ class TestGetDashboardURL(BaseTest):
     def test_get_dashboard_url(self):
         with mail.record_messages() as outbox:
 
-            dashboard_url = f'http://{app.config["FRONTEND"]}'
+            dashboard_url = f'https://{app.config["FRONTEND"]}'
             workflow = self.create_workflow('email_dashboard_url')
             self.get_workflow_api(workflow)
 
