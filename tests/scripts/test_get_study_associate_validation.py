076d198fc68878ba147880f8d8dba5272b075c42
Dan <daniel.h.funk@gmail.com>
2021-06-08 12:18:16 -0400

    fixing a failing test for study associate.

--- tests/scripts/test_get_study_associate_validation.py
+++ tests/scripts/test_get_study_associate_validation.py
@@ -5,7 +5,7 @@ from crc import app
 class TestGetStudyAssociateValidation(BaseTest):
 
     def test_get_study_associate_validation(self):
-
+        app.config['PB_ENABLED'] = True
         self.load_example_data()
         workflow = self.create_workflow('get_study_associate')
         rv = self.app.get('/v1.0/workflow-specification/%s/validate' % workflow.workflow_spec_id,
