82bf25cbc666a946d2d0832e50b9082c1fdca123
mike cullerton <michaelc@cullerton.com>
2021-07-15 13:10:51 -0400

    Test that we set file data_store values correctly when using the file_data property

--- tests/test_file_datastore.py
+++ tests/test_file_datastore.py
@@ -1,17 +1,16 @@
-import json
+from tests.base_test import BaseTest
 
+from crc import db
+from crc.models.data_store import DataStoreModel
 from crc.services.file_service import FileService
 from crc.services.workflow_processor import WorkflowProcessor
-from tests.base_test import BaseTest
-from crc.models.workflow import WorkflowStatus
-from crc import db
-from crc.api.common import ApiError
-from crc.models.task_event import TaskEventModel, TaskEventSchema
-from crc.services.workflow_service import WorkflowService
+
+from io import BytesIO
+
+import json
 
 
 class TestFileDatastore(BaseTest):
-
 
     def test_file_datastore_workflow(self):
         self.load_example_data()
@@ -32,3 +31,25 @@ class TestFileDatastore(BaseTest):
         self.assertEqual(task_data['output'], 'me')
         self.assertEqual(task_data['output2'], 'nope')
 
+    def test_file_data_store_file_data_property(self):
+        self.load_example_data()
+        workflow = self.create_workflow('enum_file_data')
+        workflow_api = self.get_workflow_api(workflow)
+        task = workflow_api.next_task
+
+        # upload the file
+        correct_name = task.form['fields'][1]['id']
+        data = {'file': (BytesIO(b"abcdef"), 'test_file.txt')}
+        rv = self.app.post('/v1.0/file?study_id=%i&workflow_id=%s&task_id=%s&form_field_key=%s' %
+                           (workflow.study_id, workflow.id, task.id, correct_name), data=data, follow_redirects=True,
+                           content_type='multipart/form-data', headers=self.logged_in_headers())
+        self.assert_success(rv)
+        file_id = json.loads(rv.get_data())['id']
+
+        # process the form that sets the datastore values
+        self.complete_form(workflow, task, {'Study_App_Doc': {'id': file_id},
+                                                     'IRB_HSR_Application_Type': {'label': 'Expedited Application'}})
+        data_store = db.session.query(DataStoreModel).filter(DataStoreModel.file_id==file_id).first()
+        self.assertEqual('IRB_HSR_Application_Type', data_store.key)
+        self.assertEqual('Expedited Application', data_store.value)
+        print('test_file_data_store_file_data_property')
