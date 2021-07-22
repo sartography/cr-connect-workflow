f6612a65d5abdf8c6cae44f07ffaf24c00d80378
Dan <daniel.h.funk@gmail.com>
2021-07-22 17:40:19 -0400

    Creating a SERVER_NAME environment variable that should help in cases where we don't have an http request (ie, a scheduled task is executing workflows) but we still want to create emails and do other things that tend to be based on a request. SERVER_NAME is a standard Flask config setting that we must have dropped along the way somewhere.

--- config/default.py
+++ config/default.py
@@ -15,8 +15,8 @@ JSON_SORT_KEYS = False  # CRITICAL.  Do not sort the data when returning values 
 API_TOKEN = environ.get('API_TOKEN', default = 'af95596f327c9ecc007b60414fc84b61')
 
 NAME = "CR Connect Workflow"
-DEFAULT_PORT = "5000"
-FLASK_PORT = environ.get('PORT0') or environ.get('FLASK_PORT', default=DEFAULT_PORT)
+SERVER_NAME = environ.get('SERVER_NAME', default="localhost:5000")
+FLASK_PORT = environ.get('PORT0') or environ.get('FLASK_PORT', default=5000)
 FRONTEND = environ.get('FRONTEND', default="localhost:4200")
 BPMN = environ.get('BPMN', default="localhost:5002")
 CORS_DEFAULT = f'{FRONTEND}, {BPMN}'
