e8ea9776a5124e7e20115de559e692cb111f101f
mike cullerton <michaelc@cullerton.com>
2021-08-02 10:04:47 -0400

    Test for creating dmn from spreadsheet

---
+++ tests/test_spreadsheet_to_dmn.py
@@ -0,0 +1,10 @@
+from tests.base_test import BaseTest
+
+class TestSStoDMN(BaseTest):
+
+    def test_ss_to_dmn(self):
+        workflow = self.create_workflow('spreadsheet_to_dmn')
+        workflow_api = self.get_workflow_api(workflow)
+        task = workflow_api.next_task
+
+        print(f'test_ss_to_dmn: task: {task}')
