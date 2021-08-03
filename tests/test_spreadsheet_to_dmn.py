abe258cdf164932dc7bf5fb2d1cefaa0eea03cda
mike cullerton <michaelc@cullerton.com>
2021-08-03 11:56:53 -0400

    Added example data so we can grab a workflow spec

--- tests/test_spreadsheet_to_dmn.py
+++ tests/test_spreadsheet_to_dmn.py
@@ -1,8 +1,10 @@
 from tests.base_test import BaseTest
 
+
 class TestSStoDMN(BaseTest):
 
     def test_ss_to_dmn(self):
+        self.load_example_data()
         workflow = self.create_workflow('spreadsheet_to_dmn')
         workflow_api = self.get_workflow_api(workflow)
         task = workflow_api.next_task
