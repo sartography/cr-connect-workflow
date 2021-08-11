05e7f5e2532fbc28b66c4ba5fe99efaea713f50b
mike cullerton <michaelc@cullerton.com>
2021-08-11 10:06:12 -0400

    Worksheet for Alex

--- crc/scripts/dmn_from_spreadsheet.py
+++ crc/scripts/dmn_from_spreadsheet.py
@@ -36,7 +36,8 @@ class DMNFromSpreadSheet(Script):
         # ss_file_path = os.path.join(app.root_path, 'static', 'spreadsheet_to_dmn.xlsx')
         # ss_file_path = os.path.join(app.root_path, 'static', 'New_test_budget_spreadsheet.xlsx')
         # ss_file_path = os.path.join(app.root_path, 'static', 'large_test_spreadsheet.xlsx')
-        ss_file_path = os.path.join(app.root_path, 'static', 'DMN_Upload_CRC_Orgs.xlsx')
+        # ss_file_path = os.path.join(app.root_path, 'static', 'DMN_Upload_CRC_Orgs.xlsx')
+        ss_file_path = os.path.join(app.root_path, 'static', 'DMN_Upload_Org_Enums.xlsx')
         df = pd.read_excel(ss_file_path, header=None)
 
         root = etree.Element("definitions",
