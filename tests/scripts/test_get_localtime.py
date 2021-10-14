fc3e7f818324894934c7a1c4cbaca07326b3c888
mike cullerton <michaelc@cullerton.com>
2021-10-14 11:02:51 -0400

    Simple test for get_localtime script

--- tests/scripts/test_get_localtime.py
+++ tests/scripts/test_get_localtime.py
@@ -1,21 +1,17 @@
 from tests.base_test import BaseTest
-from crc.models.file import FileDataModel
-from crc import session
+from crc.scripts.get_localtime import GetLocaltime
 
 
 class TestGetLocaltime(BaseTest):
 
     def test_get_localtime(self):
         self.load_example_data()
-        # file_model = session.query(FileDataModel).first()
-        # test_time = file_model.date_created
 
         workflow = self.create_workflow('get_localtime')
         workflow_api = self.get_workflow_api(workflow)
         task = workflow_api.next_task
-        # workflow_api = self.complete_form(workflow, task, {'timestamp': test_time})
 
-        # local_time =
+        timestamp = task.data['timestamp']
+        localtime = task.data['localtime']
 
-        print('test_get_localtime')
-
+        self.assertEqual(localtime, GetLocaltime().do_task(None, None, None, timestamp=timestamp))
