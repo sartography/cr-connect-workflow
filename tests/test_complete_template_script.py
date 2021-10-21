383bb964d1ddef1ac182d13b33ff35a357cd273b
mike cullerton <michaelc@cullerton.com>
2021-10-21 10:31:02 -0400

    Use the new JinjaService

--- tests/test_complete_template_script.py
+++ tests/test_complete_template_script.py
@@ -4,19 +4,20 @@ import copy
 from docxtpl import Listing
 
 from crc.scripts.complete_template import CompleteTemplate
+from crc.services.jinja_service import JinjaService
 
 
 class TestCompleteTemplate(unittest.TestCase):
 
     def test_rich_text_update(self):
-        script = CompleteTemplate()
+        script = JinjaService()
         data = {"name": "Dan"}
         data_copy = copy.deepcopy(data)
         script.rich_text_update(data_copy)
         self.assertEqual(data, data_copy)
 
     def test_rich_text_update_new_line(self):
-        script = CompleteTemplate()
+        script = JinjaService()
         data = {"name": "Dan\n Funk"}
         data_copy = copy.deepcopy(data)
         script.rich_text_update(data_copy)
@@ -24,7 +25,7 @@ class TestCompleteTemplate(unittest.TestCase):
         self.assertIsInstance(data_copy["name"], Listing)
 
     def test_rich_text_nested_new_line(self):
-        script = CompleteTemplate()
+        script = JinjaService()
         data = {"names": [{"name": "Dan\n Funk"}]}
         data_copy = copy.deepcopy(data)
         script.rich_text_update(data_copy)
