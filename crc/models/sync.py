2bf4364d02f4ba83f0b05b197493f73dd7cb4b65
mike cullerton <michaelc@cullerton.com>
2021-11-22 11:07:27 -0500

    New attributes, and to_dict method

--- crc/models/sync.py
+++ crc/models/sync.py
@@ -10,13 +10,33 @@ class SyncSourceModel(db.Model):
 
 class SyncWorkflow(object):
 
-    def __init__(self, index=None, workflow_spec_id=None, date_created=None, md5_hash=None, name=None, new=None):
+    def __init__(self,
+                 index=None,
+                 workflow_spec_id=None,
+                 date_created=None,
+                 md5_hash=None,
+                 location=None,
+                 name=None,
+                 new=None,
+                 level_0=None):
         self.index = index
         self.workflow_spec_id = workflow_spec_id
         self.date_created = date_created
         self.md5_hash = md5_hash
+        self.location = location
         self.name = name
         self.new = new
+        self.level_0 = level_0
+
+    def to_dict(self):
+        return {'index': self.index,
+                'workflow_spec_id': self.workflow_spec_id,
+                'date_created': self.date_created,
+                'md5_hash': self.md5_hash,
+                'location': self.location,
+                'name': self.name,
+                'new': self.new,
+                'level_0': self.level_0}
 
 
 class SyncWorkflowSchema(ma.Schema):
