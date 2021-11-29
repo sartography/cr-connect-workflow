86b3417742bed43c7fe1fb4eb3f04867ef6ae7c2
mike cullerton <michaelc@cullerton.com>
2021-11-29 10:35:21 -0500

    Some basic tests

--- tests/test_workflow_sync_service.py
+++ tests/test_workflow_sync_service.py
@@ -2,6 +2,8 @@ from tests.base_test import BaseTest
 
 from crc import app, session
 from crc.api.workflow_sync import get_sync_sources, WorkflowSyncService
+from crc.models.file import FileDataModel
+from crc.models.sync import SyncFile, SyncWorkflow
 from crc.models.workflow import WorkflowSpecModel
 
 
@@ -16,6 +18,8 @@ def setup_test_environ():
 
 class TestWorkflowSyncService(BaseTest):
 
+    remote = 'http://localhost:5000'
+
     def test_sync_sources_from_env(self):
         setup_test_environ()
         parsed = get_sync_sources()
@@ -24,9 +28,12 @@ class TestWorkflowSyncService(BaseTest):
                          parsed)
 
     def test_get_master_list(self):
+        self.load_example_data()
+        # setup_test_environ()
+        # TODO: Somehow, this needs to be mocked or local
         # remote = 'https://testing.crconnect.uvadcos.io'
-        remote = 'localhost:3000'
-        master_list = WorkflowSyncService.get_master_list(remote)
+        # remote = 'http://localhost:5000'
+        master_list = WorkflowSyncService.get_master_list(self.remote)
 
         print(f'test_get_master_list: master_list: {master_list}')
 
@@ -34,18 +41,21 @@ class TestWorkflowSyncService(BaseTest):
         pass
 
     def test_get_changed_workflows(self):
-        pass
+        changed_workflows = WorkflowSyncService.get_changed_workflows(self.remote,
+                                                                      as_df=False,
+                                                                      keep_new_local=False)
+        self.assertGreater(len(changed_workflows), 0)
+        self.assertIsInstance(changed_workflows[0], SyncWorkflow)
 
     def test_sync_changed_files(self):
         pass
 
     def test_get_changed_files(self):
         self.load_example_data()
-        remote = 'localhost:3000'
         workflow_spec_model = session.query(WorkflowSpecModel).first()
-        changed_files = WorkflowSyncService.get_changed_files(remote, workflow_spec_model.id, as_df=False)
-
-        print('test_get_changed_files')
+        changed_files = WorkflowSyncService.get_changed_files(self.remote, workflow_spec_model.id, as_df=False)
+        self.assertEqual(1, len(changed_files))
+        self.assertIsInstance(changed_files[0], SyncFile)
 
     def test_get_all_spec_state(self):
         pass
@@ -54,5 +64,5 @@ class TestWorkflowSyncService(BaseTest):
         self.load_example_data()
         workflow_spec_model = session.query(WorkflowSpecModel).first()
         workflow_spec_files = WorkflowSyncService.get_workflow_spec_files(workflow_spec_model.id)
-
-        print('test_get_workflow_spec_files')
+        self.assertEqual(1, len(workflow_spec_files))
+        self.assertIsInstance(workflow_spec_files[0], FileDataModel)
