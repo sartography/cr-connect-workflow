bf21f78085a92a3e137024bce567bc266f4cdab7
mike cullerton <michaelc@cullerton.com>
2021-12-10 16:27:17 -0500

    Seeded progress_status column when status is in_progress

--- migrations/versions/d830959e96c0_new_study_progress_statuses.py
+++ migrations/versions/d830959e96c0_new_study_progress_statuses.py
@@ -20,6 +20,7 @@ depends_on = None
 def upgrade():
     op.execute("CREATE TYPE progressstatus AS ENUM('in_progress', 'submitted_for_pre_review', 'in_pre_review', 'returned_from_pre_review', 'pre_review_complete', 'agenda_date_set', 'approved', 'approved_with_conditions', 'deferred', 'disapproved')")
     op.add_column('study', sa.Column('progress_status', sa.Enum('in_progress', 'submitted_for_pre_review', 'in_pre_review', 'returned_from_pre_review', 'pre_review_complete', 'agenda_date_set', 'approved', 'approved_with_conditions', 'deferred', 'disapproved', name='progressstatus'), nullable=True))
+    op.execute("update study set progress_status = 'in_progress' where status='in_progress'")
 
 
 def downgrade():
