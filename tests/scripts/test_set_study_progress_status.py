daef419993318589cbef9382362006e2e7498c7f
mike cullerton <michaelc@cullerton.com>
2021-12-10 16:18:05 -0500

    Tests and workflow for the changes

--- tests/scripts/test_set_study_progress_status.py
+++ tests/scripts/test_set_study_progress_status.py
@@ -1,14 +1,16 @@
 from tests.base_test import BaseTest
 
+from crc.models.study import ProgressStatus
 
-class TestSetStudyStatus(BaseTest):
 
-    def test_set_study_status_validation(self):
+class TestSetStudyProgressStatus(BaseTest):
+
+    def test_set_study_progress_status_validation(self):
         self.load_example_data()
-        spec_model = self.load_test_spec('set_study_status')
+        spec_model = self.load_test_spec('set_study_progress_status')
         rv = self.app.get('/v1.0/workflow-specification/%s/validate' % spec_model.id, headers=self.logged_in_headers())
         # The workflow has an enum option that causes an exception.
-        # We take advantage of this in test_set_study_status_fail below.
+        # We take advantage of this in test_set_study_progress_status_fail below.
         # Sometimes, the validation process chooses the failing path,
         # so we have to check for that here.
         try:
@@ -17,8 +19,9 @@ class TestSetStudyStatus(BaseTest):
             # 'asdf' is the failing enum option
             self.assertEqual('asdf', rv.json[0]['task_data']['selected_status'])
 
-    def test_set_study_status(self):
-        workflow = self.create_workflow('set_study_status')
+    def test_set_study_progress_status(self):
+        workflow = self.create_workflow('set_study_progress_status')
+        workflow.study.progress_status = ProgressStatus.in_progress
         workflow_api = self.get_workflow_api(workflow)
         task = workflow_api.next_task
 
@@ -32,10 +35,10 @@ class TestSetStudyStatus(BaseTest):
         self.assertEqual('disapproved', task.data['selected_status'])
         self.assertEqual('disapproved', task.data['new_status'])
 
-    def test_set_study_status_fail(self):
+    def test_set_study_progress_status_fail(self):
 
         self.load_example_data()
-        workflow = self.create_workflow('set_study_status')
+        workflow = self.create_workflow('set_study_progress_status')
         workflow_api = self.get_workflow_api(workflow)
         task = workflow_api.next_task
 
