e715980bca6dd674d819f54d935b7bfa1eb29397
Dan <daniel.h.funk@gmail.com>
2021-12-21 15:01:07 -0500

    bumping SpiffWorkflow to account for an issue when dealing with a subprocess immediately before a looping back to a previous task.

--- tests/test_user_roles.py
+++ tests/test_user_roles.py
@@ -185,7 +185,7 @@ class TestUserRoles(BaseTest):
         nav = workflow_api.navigation
         self.assertEqual('READY', nav[1].state)  # When you loop back the task is again in the ready state.
         self.assertEqual('LOCKED', nav[2].state)  # Second item is locked, it is the review and doesn't belong to this user.
-        self.assertEqual('MAYBE', nav[3].state)  # Feedback is now in a Maybe state, because we might head there again.
+        self.assertEqual('COMPLETED', nav[3].state)
         self.assertEqual('READY', workflow_api.next_task.state)
 
         data["favorite_color"] = "blue"
