a44300bd969d024aaa982bf618eaf10976f5e1b0
mike cullerton <michaelc@cullerton.com>
2022-01-04 07:24:21 -0500

    More upgrade code - delete associated data for each processed file

--- migrations/versions/7225d990740e_move_files_to_filesystem.py
+++ migrations/versions/7225d990740e_move_files_to_filesystem.py
@@ -8,10 +8,13 @@ Create Date: 2021-12-14 10:52:50.785342
 
 from alembic import op
 import sqlalchemy as sa
+
+import crc
 from crc import app, session
-from crc.models.file import FileModel, FileDataModel
+from crc.models.file import FileModel, FileDataModel, LookupFileModel
+from crc.models.workflow import WorkflowSpecDependencyFile
 from crc.services.file_service import FileService
-from crc.services.temp_migration_service import FromFilesystemService, ToFilesystemService
+from crc.services.temp_migration_service import FromFilesystemService, ToFilesystemService, SYNC_FILE_ROOT
 
 import os
 
@@ -33,27 +36,46 @@ def upgrade():
     """
 
     processed_files = []
-    files = session.query(FileModel).all()
+    # Process workflow spec files
+    files = session.query(FileModel).filter(FileModel.workflow_spec_id is not None).all()
     for file in files:
         if file.archived is not True:
             ToFilesystemService().write_file_to_system(file)
             processed_files.append(file.id)
 
-    # TODO: delete processed files from file_data table
+    # TODO:
+    #  delete processed files from file_data table
+    #  delete workflow_spec_dependency_file table
+    #  delete file_data_model_id from lookup_file table
     for file_id in processed_files:
         processed_models = session.query(FileDataModel).filter(FileDataModel.file_model_id==file_id).all()
         for processed_model in processed_models:
+            dependency = session.query(WorkflowSpecDependencyFile).filter(WorkflowSpecDependencyFile.file_data_id==processed_model.id).first()
+            if dependency is not None:
+                session.delete(dependency)
+            lookups = session.query(LookupFileModel).filter(LookupFileModel.file_data_model_id==processed_model.id).all()
+            for lookup in lookups:
+                lookup.file_data_model_id = None
             session.delete(processed_model)
         print(f'upgrade: in processed files: file_id: {file_id}')
 
-    # session.commit()
+        session.commit()
+
+    op.drop_table('workflow_spec_dependency_file')
     print('upgrade: done: ')
 
 
 def downgrade():
 
     # TODO: This is a work in progress, and depends on what we do in upgrade()
-    SYNC_FILE_ROOT = os.path.join(app.root_path, '..', 'files')
+    op.create_table('workflow_spec_dependency_file',
+    sa.Column('file_data_id', sa.Integer(), nullable=False),
+    sa.Column('workflow_id', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['file_data_id'], ['file_data.id'], ),
+    sa.ForeignKeyConstraint(['workflow_id'], ['workflow.id'], ),
+    sa.PrimaryKeyConstraint('file_data_id', 'workflow_id')
+    )
+
     FromFilesystemService().update_file_metadata_from_filesystem(SYNC_FILE_ROOT)
 
     print('downgrade: ')
