2ecf6cdf86b7895ba210eb321a28af1ba1cf4afb
mike cullerton <michaelc@cullerton.com>
2022-01-24 11:03:49 -0500

    Validation is the right way to test this.

--- tests/workflow/test_workflow_missing_library.py
+++ tests/workflow/test_workflow_missing_library.py
@@ -9,10 +9,8 @@ class TestMissingLibrary(BaseTest):
         """We call a library that does not exist, and
         test to see if our error service hint is in the error message."""
         workflow = self.create_workflow('missing_library')
-        url = f'/v1.0/workflow/{workflow.id}?do_engine_steps=True'
-        rv = self.app.get(url,
-                          headers=self.logged_in_headers(),
-                          content_type="application/json")
+        rv = self.app.get('/v1.0/workflow-specification/%s/validate' % workflow.workflow_spec_id, headers=self.logged_in_headers())
         json_data = json.loads(rv.get_data(as_text=True))
-        self.assertEqual('workflow_validation_error', json_data['code'])
-        self.assertIn("'Process_Multiply' was not found", json_data['message'])
+        self.assertEqual('workflow_validation_error', json_data[0]['code'])
+        self.assertIn("'Process_Multiply' was not found", json_data[0]['message'])
+        self.assertEqual('The workflow spec could not be parsed. If you are loading a library, check whether the name is correct.', json_data[0]['hint'])
