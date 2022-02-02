6f233a9b793e353341114d1175757e27c9fe8d12
mike cullerton <michaelc@cullerton.com>
2022-02-02 12:51:14 -0500

    removed unused code

--- crc/models/git_models.py
+++ crc/models/git_models.py
@@ -9,22 +9,20 @@ class GitRepo(object):
         instance.directory = repo.working_dir
         instance.branch = repo.active_branch.name
         instance.merge_branch = app.config['GIT_MERGE_BRANCH']
-        # instance.status = repo.index.diff(repo.head.commit)
-        # instance.user =
         instance.changes = [item.a_path for item in repo.index.diff(None)]
         instance.untracked = repo.untracked_files
 
         return instance
 
 
-class GitCommit(object):
-    pass
-
-
 class GitRepoSchema(ma.Schema):
     class Meta:
         model = GitRepo
         fields = ["directory", "branch", "merge_branch", "changes", "untracked"]
+
+
+class GitCommit(object):
+    pass
 
 
 class GitCommitSchema(ma.Schema):
