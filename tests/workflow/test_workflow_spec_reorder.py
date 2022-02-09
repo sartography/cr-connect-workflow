d46dfdec4004816fd291c09b47cfe81d2a3701c6
mike cullerton <michaelc@cullerton.com>
2022-02-09 12:14:37 -0500

    Fixed the bad order test

--- tests/workflow/test_workflow_spec_reorder.py
+++ tests/workflow/test_workflow_spec_reorder.py
@@ -131,53 +131,53 @@ class TestWorkflowSpecReorder(BaseTest):
         self.assertEqual(specs[2].id, new_specs[2].id)
         self.assertEqual(specs[3].id, new_specs[3].id)
 
-    # def test_workflow_spec_reorder_bad_order(self):
-    #     rv_1, rv_2, rv_3 = self._load_sample_workflow_specs()
-    #     category_id = rv_1.json['category_id']
-    #
-    #     specs = WorkflowSpecService().get_specs()
-    #     specs.sort(key=lambda w: w.display_order)
-    #
-    #     # Set bad display_orders
-    #     spec_model = specs[0]
-    #     spec_model.display_order = 1
-    #     WorkflowSpecService().update_spec(spec_model)
-    #     # session.add(spec_model)
-    #     spec_model = specs[1]
-    #     spec_model.display_order = 1
-    #     WorkflowSpecService().update_spec(spec_model)
-    #     # session.add(spec_model)
-    #     spec_model = specs[2]
-    #     spec_model.display_order = 1
-    #     WorkflowSpecService().update_spec(spec_model)
-    #     # session.add(spec_model)
-    #     # session.commit()
-    #
-    #     bad_specs = WorkflowSpecService().get_specs()
-    #     bad_specs.sort(key=lambda w: w.display_order)
-    #
-    #     self.assertEqual(1, bad_specs[0].display_order)
-    #     self.assertEqual('test_spec_2', bad_specs[0].id)
-    #     self.assertEqual(1, bad_specs[1].display_order)
-    #     self.assertEqual('random_fact', bad_specs[1].id)
-    #     self.assertEqual(1, bad_specs[2].display_order)
-    #     self.assertEqual('test_spec_1', bad_specs[2].id)
-    #     self.assertEqual(3, bad_specs[3].display_order)
-    #     self.assertEqual('test_spec_3', bad_specs[3].id)
-    #
-    #     # Move test_spec_2 up
-    #     # This should cause a cleanup of the bad display_order numbers
-    #     rv = self.app.put(f"/v1.0/workflow-specification/test_spec_2/reorder?direction=up",
-    #                       headers=self.logged_in_headers())
-    #
-    #     # After moving 2 up, the order should be
-    #     # test_spec_1, test_spec_2, random_fact, test_spec_3
-    #     # Make sure we have good display_order numbers too
-    #     self.assertEqual('test_spec_1', rv.json[0]['id'])
-    #     self.assertEqual(0, rv.json[0]['display_order'])
-    #     self.assertEqual('test_spec_2', rv.json[1]['id'])
-    #     self.assertEqual(1, rv.json[1]['display_order'])
-    #     self.assertEqual('random_fact', rv.json[2]['id'])
-    #     self.assertEqual(2, rv.json[2]['display_order'])
-    #     self.assertEqual('test_spec_3', rv.json[3]['id'])
-    #     self.assertEqual(3, rv.json[3]['display_order'])
+    def test_workflow_spec_reorder_bad_order(self):
+        rv_1, rv_2, rv_3 = self._load_sample_workflow_specs()
+        category_id = rv_1.json['category_id']
+
+        specs = WorkflowSpecService().get_specs()
+        specs.sort(key=lambda w: w.display_order)
+
+        # Set bad display_orders
+        spec_model = specs[0]
+        spec_model.display_order = 1
+        WorkflowSpecService().update_spec(spec_model)
+        # session.add(spec_model)
+        spec_model = specs[1]
+        spec_model.display_order = 1
+        WorkflowSpecService().update_spec(spec_model)
+        # session.add(spec_model)
+        spec_model = specs[2]
+        spec_model.display_order = 1
+        WorkflowSpecService().update_spec(spec_model)
+        # session.add(spec_model)
+        # session.commit()
+
+        bad_specs = WorkflowSpecService().get_specs()
+        bad_specs.sort(key=lambda w: w.display_order)
+
+        self.assertEqual(1, bad_specs[0].display_order)
+        self.assertEqual('test_spec_2', bad_specs[0].id)
+        self.assertEqual(1, bad_specs[1].display_order)
+        self.assertEqual('random_fact', bad_specs[1].id)
+        self.assertEqual(1, bad_specs[2].display_order)
+        self.assertEqual('test_spec_1', bad_specs[2].id)
+        self.assertEqual(3, bad_specs[3].display_order)
+        self.assertEqual('test_spec_3', bad_specs[3].id)
+
+        # Move test_spec_2 up
+        # This should cause a cleanup of the bad display_order numbers
+        rv = self.app.put(f"/v1.0/workflow-specification/test_spec_2/reorder?direction=up",
+                          headers=self.logged_in_headers())
+
+        # After moving 2 up, the order should be
+        # test_spec_1, test_spec_2, random_fact, test_spec_3
+        # Make sure we have good display_order numbers too
+        self.assertEqual('test_spec_2', rv.json[0]['id'])
+        self.assertEqual(0, rv.json[0]['display_order'])
+        self.assertEqual('random_fact', rv.json[1]['id'])
+        self.assertEqual(1, rv.json[1]['display_order'])
+        self.assertEqual('test_spec_1', rv.json[2]['id'])
+        self.assertEqual(2, rv.json[2]['display_order'])
+        self.assertEqual('test_spec_3', rv.json[3]['id'])
+        self.assertEqual(3, rv.json[3]['display_order'])
