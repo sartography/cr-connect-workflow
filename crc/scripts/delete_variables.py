8c66a35f36de1724e82b2158f9c890cfc91b7fee
mike cullerton <michaelc@cullerton.com>
2022-02-21 12:55:03 -0500

    Script to delete variables from task data

---
+++ crc/scripts/delete_variables.py
@@ -0,0 +1,17 @@
+from crc.api.common import ApiError
+from crc.scripts.script import Script
+
+
+class DeleteVariables(Script):
+
+    def get_description(self):
+        return """Script to delete variables from task_data, if they exist.
+        Accepts a list of variables to delete."""
+
+    def do_task_validate_only(self, task, study_id, workflow_id, *args, **kwargs):
+        return self.do_task(task, study_id, workflow_id, *args, **kwargs)
+
+    def do_task(self, task, study_id, workflow_id, *args, **kwargs):
+        for arg in args:
+            if arg in task.data:
+                del(task.data[arg])
