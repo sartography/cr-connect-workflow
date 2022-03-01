e416873916dd2e83d1fc6dae57924ced354b1e00
alicia pritchett <aliciateaford@gmail.com>
2022-03-01 14:45:18 -0500

    Fixes #650 - Approvals not Clearing When Completed

--- crc/services/workflow_service.py
+++ crc/services/workflow_service.py
@@ -923,6 +923,8 @@ class WorkflowService(object):
         db.session.query(TaskEventModel). \
             filter(TaskEventModel.workflow_id == processor.workflow_model.id). \
             filter(TaskEventModel.action == WorkflowService.TASK_ACTION_ASSIGNMENT).delete()
+        db.session.commit()
+
         tasks = processor.get_current_user_tasks()
         for task in tasks:
             user_ids = WorkflowService.get_users_assigned_to_task(processor, task)
