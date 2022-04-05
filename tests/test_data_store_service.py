946e74118f02bf1cae1a2eec76d8ded823808ee6
mike cullerton <michaelc@cullerton.com>
2022-04-05 18:17:31 -0400

    Test updating records Test deleting records when setting with empty value Test deleting 'extra' records

--- tests/test_data_store_service.py
+++ tests/test_data_store_service.py
@@ -7,6 +7,8 @@ from crc.models.user import UserModel
 from crc.services.workflow_service import WorkflowService
 from flask import g
 
+import time
+
 
 class TestDataStoreValidation(BaseTest):
 
@@ -61,6 +63,57 @@ class TestDataStoreValidation(BaseTest):
 
         session.commit()
 
+    @staticmethod
+    def add_multiple_records(file_id):
+        dsm = DataStoreModel(
+            key='some_key',
+            workflow_id=None,
+            study_id=None,
+            task_spec=None,
+            spec_id=None,
+            user_id=None,
+            file_id=file_id,
+            value='previous_file_data_value_1'
+        )
+        session.add(dsm)
+        session.commit()
+        time.sleep(1)
+
+        dsm = DataStoreModel(
+            key='some_key',
+            workflow_id=None,
+            study_id=None,
+            task_spec=None,
+            spec_id=None,
+            user_id=None,
+            file_id=file_id,
+            value='previous_file_data_value_2'
+        )
+        session.add(dsm)
+        session.commit()
+        time.sleep(1)
+
+        dsm = DataStoreModel(
+            key='some_key',
+            workflow_id=None,
+            study_id=None,
+            task_spec=None,
+            spec_id=None,
+            user_id=None,
+            file_id=file_id,
+            value='previous_file_data_value_3'
+        )
+        session.add(dsm)
+        session.commit()
+
+    def run_data_store_set(self, form_data):
+        workflow = self.create_workflow('data_store_set')
+        workflow_api = self.get_workflow_api(workflow)
+        task = workflow_api.next_task
+        self.complete_form(workflow, task, form_data)
+        result = session.query(DataStoreModel).all()
+        return result
+
     def test_data_store_validation(self):
         # add_studies also adds test users
         self.add_studies()
@@ -92,3 +145,91 @@ class TestDataStoreValidation(BaseTest):
         self.assertEqual('bad_file_data_value', result['bad_file_data_value'])
         self.assertIn('bad_user_data_value', result)
         self.assertEqual('bad_user_data_value', result['bad_user_data_value'])
+
+    def test_set_reset_data_store(self):
+        """Add records to the data store,
+           then set new values for those records.
+           Assert that we don't create new records.
+           Assert that we have the new values"""
+        file_id = self.add_test_file()
+
+        form_data = {'key': 'my_key', 'value': 'my_value', 'file_id': file_id}
+        result = self.run_data_store_set(form_data)
+
+        self.assertEqual(3, len(result))
+        for record in result:
+            self.assertEqual('my_value', record.value)
+
+        form_data = {'key': 'my_key', 'value': 'my_new_value', 'file_id': file_id}
+        result = self.run_data_store_set(form_data)
+
+        self.assertEqual(3, len(result))
+        for record in result:
+            self.assertEqual('my_new_value', record.value)
+
+    def test_reset_datastore_different_workflow(self):
+        """Add records to the data store,
+           then set new values for those records from a different workflow.
+           Assert that we don't create new records.
+           Assert that we have the new values"""
+        file_id = self.add_test_file()
+
+        form_data = {'key': 'some_key', 'value': 'some_value', 'file_id': file_id}
+        result = self.run_data_store_set(form_data)
+
+        self.assertEqual(3, len(result))
+        for record in result:
+            self.assertEqual('some_value', record.value)
+
+        workflow = self.create_workflow('data_store_set_2')
+        workflow_api = self.get_workflow_api(workflow)
+        task = workflow_api.next_task
+
+        form_data = {'key': 'some_key', 'value': 'some_new_value', 'file_id': file_id}
+        self.complete_form(workflow, task, form_data)
+
+        result = session.query(DataStoreModel).all()
+        self.assertEqual(3, len(result))
+        for record in result:
+            self.assertEqual('some_new_value', record.value)
+
+    def test_delete_extra_records(self):
+        """We had a bug where we created new records instead of updating existing records
+           This ensures we remove 'extra' records"""
+        file_id = self.add_test_file()
+        self.add_multiple_records(file_id)
+
+        result = session.query(DataStoreModel).filter(DataStoreModel.file_id == file_id).all()
+        self.assertEqual(3, len(result))
+        previous_values = ['previous_file_data_value_1', 'previous_file_data_value_2', 'previous_file_data_value_3']
+        for record in result:
+            self.assertIn(record.value, previous_values)
+
+        form_data = {'key': 'some_key', 'value': 'some_value', 'file_id': file_id}
+        result = self.run_data_store_set(form_data)
+
+        self.assertEqual(3, len(result))
+        for record in result:
+            self.assertEqual('some_value', record.value)
+
+    def test_delete_record_on_empty_value(self):
+        """If we set a data store with an empty string,
+           assert that we delete the record."""
+        file_id = self.add_test_file()
+
+        form_data = {'key': 'my_key', 'value': 'my_value', 'file_id': file_id}
+        result = self.run_data_store_set(form_data)
+
+        self.assertEqual(3, len(result))
+        for record in result:
+            self.assertEqual('my_value', record.value)
+
+        workflow = self.create_workflow('data_store_set')
+        workflow_api = self.get_workflow_api(workflow)
+        task = workflow_api.next_task
+
+        re_form_data = {'key': 'my_key', 'value': '', 'file_id': file_id}
+        self.complete_form(workflow, task, re_form_data)
+
+        result = session.query(DataStoreModel).all()
+        self.assertEqual(0, len(result))
