2b5f7c2166308f2a809857f9f03bbe9c786a3893
jasquat <jasquat@users.noreply.github.com>
2022-05-05 13:40:59 -0400

    remove unneeded docker run command in favor of docker compose

--- postgres/start.sh
+++ postgres/start.sh
@@ -10,12 +10,9 @@ set -o errtrace -o errexit -o nounset -o pipefail
 docker stop postgres_db_1 || echo ''
 mkdir -p "${HOME}/docker/volumes/postgres"
 
-#docker pull postgres:latest
-#docker run --rm   --name pg-docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data  postgres
-
 if command -v docker-compose >/dev/null ; then
-  docker-compose -f docker-compose.yml up --no-start
-  docker-compose -f docker-compose.yml start
+  docker-compose -f postgres/docker-compose.yml up --no-start
+  docker-compose -f postgres/docker-compose.yml start
 else
   docker compose -f postgres/docker-compose.yml up --no-start
   docker compose -f postgres/docker-compose.yml start
