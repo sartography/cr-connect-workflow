1b4821d2b901a77a3b524d9082b529b063e5408c
mike cullerton <michaelc@cullerton.com>
2022-05-17 09:59:33 -0400

    Use a new INSTANCE_NAME variable for this, instead of SERVER_NAME

--- crc/scripts/get_instance.py
+++ crc/scripts/get_instance.py
@@ -5,14 +5,14 @@ from crc.scripts.script import Script
 class GetInstance(Script):
 
     def get_description(self):
-        return """Get the name of the current instance, using the SERVER_NAME environment variable."""
+        return """Get the name of the current instance, using the INSTANCE_NAME environment variable."""
 
     def do_task_validate_only(self, task, study_id, workflow_id, *args, **kwargs):
         return self.do_task(task, study_id, workflow_id, *args, **kwargs)
 
     def do_task(self, task, study_id, workflow_id, *args, **kwargs):
-        if 'SERVER_NAME' in app.config:
-            return app.config['SERVER_NAME']
+        if 'INSTANCE_NAME' in app.config:
+            return app.config['INSTANCE_NAME']
         # TODO: Not sure what we should do here
-        app.logger.info('no_server_name: SERVER_NAME not configured for this server.')
+        app.logger.info('no_instance_name: INSTANCE_NAME not configured for this server.')
         return ''
