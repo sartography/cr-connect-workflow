26abac3352f78f4a173ddce1e27bc17c7a0d2229
mike cullerton <michaelc@cullerton.com>
2022-05-17 10:00:16 -0400

    Update the test and README

--- tests/scripts/test_get_instance.py
+++ tests/scripts/test_get_instance.py
@@ -6,9 +6,9 @@ from crc import app
 class TestGetInstance(BaseTest):
 
     def test_get_instance(self):
-        server_name = app.config['SERVER_NAME']
+        instance_name = app.config['INSTANCE_NAME']
         workflow = self.create_workflow('get_instance')
         workflow_api = self.get_workflow_api(workflow)
         task = workflow_api.next_task
         data = task.data
-        self.assertEqual(server_name, data['instance'])
+        self.assertEqual(instance_name, data['instance'])
