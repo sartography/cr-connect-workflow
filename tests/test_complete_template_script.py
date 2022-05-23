e92be44b6cb31352f17c07097701dfa90bf54c02
mike cullerton <michaelc@cullerton.com>
2022-05-23 14:22:14 -0400

    Assert that we have the correct file name, depending on whether we pass the optional file_name parameter Modify the bpmn file to accept the optional file_name

--- tests/test_complete_template_script.py
+++ tests/test_complete_template_script.py
@@ -39,25 +39,45 @@ class TestCompleteTemplate(unittest.TestCase):
 
 class TestEmbeddedTemplate(BaseTest):
 
-    def test_embedded_template(self):
+    def run_docx_embedded_workflow(self, data):
         self.create_reference_document()
         workflow = self.create_workflow('docx_embedded')
         workflow_api = self.get_workflow_api(workflow)
         task = workflow_api.next_task
 
+        workflow_api = self.complete_form(workflow, task, data)
+        return workflow_api
+
+    def test_embedded_template(self):
         data = {'include_me': 'Hello {{ name }}!',
                 'name': 'World',
-                'file_name': 'simple.docx',
+                'template_file_name': 'simple.docx',
                 'irb_doc_code': 'Study_App_Doc'}
-        self.complete_form(workflow, task, data)
+        workflow_api = self.run_docx_embedded_workflow(data)
 
         # Get the file data created for us in the workflow
         file_model = session.query(FileModel).\
-            filter(FileModel.workflow_id == workflow.id).\
+            filter(FileModel.workflow_id == workflow_api.id).\
             filter(FileModel.irb_doc_code == 'Study_App_Doc').\
             first()
+        # If we don't pass file_name, name should be set to template_file_name
+        self.assertEqual(data['template_file_name'], file_model.name)
 
         # read the data as a word document
         document = docx.Document(BytesIO(file_model.data))
         # Make sure 'Hello World!' is there
         self.assertEqual('Hello World!', document.paragraphs[4].text)
+
+        data = {'include_me': 'Hello {{ name }}!',
+                'name': 'World',
+                'template_file_name': 'simple.docx',
+                'irb_doc_code': 'Study_App_Doc',
+                'file_name': 'test_file_name.docx'}
+        workflow_api = self.run_docx_embedded_workflow(data)
+
+        file_model = session.query(FileModel).\
+            filter(FileModel.workflow_id == workflow_api.id).\
+            filter(FileModel.irb_doc_code == 'Study_App_Doc').\
+            first()
+        # If we do pass file_name, name should be set to file_name
+        self.assertEqual(data['file_name'], file_model.name)
