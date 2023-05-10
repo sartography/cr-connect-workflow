0e26f46a051755f944b264b3fcd871c6a160a8fb
danfunk <daniel.h.funk@gmail.com>
2023-05-10 11:49:34 -0400

    minor tweaks to postgres local development.

--- postgres/connect.sh
+++ postgres/connect.sh
@@ -1,4 +1,4 @@
 #psql -h localhost -U postgres -d postgres
 #psql -h localhost -U crc_user -d crc_test
 #psql -h localhost -U crc_user -d crc_dev
-docker exec -it postgres_db_1 psql -U crc_user crc_dev
+docker exec -it postgres-db-1 psql -U crc_user crc_dev
