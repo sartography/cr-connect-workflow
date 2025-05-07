3c1315d7ff628164ac5efba79d6305993212e2df
mike cullerton <webstuff@bakednotfried.com>
2025-05-07 08:34:09 -0400

    save dsp data for merge into dsp document

---
+++ crc/scripts/set_dsp_data.py
@@ -0,0 +1,21 @@
+from crc.api.common import ApiError
+from crc.scripts.script import Script
+
+
+class SaveDSPData(Script):
+
+    def get_description(self):
+        return """Saves data from the DSP forms to the data store.
+        Which fields we save is determined by the dsp_form_variables reference file"""
+
+    def do_task_validate_only(self, task, study_id, workflow_id, *args, **kwargs):
+        return self.do_task(task, study_id, workflow_id, *args, **kwargs)
+
+    def do_task(self, task, study_id, workflow_id, *args, **kwargs):
+        doc_dict = self.get_reference_file_as_dictionary()
+        for template_variable in doc_dict:
+            if template_variable and template_variable in task.data:
+                key = doc_dict[template_variable]['stored_variable']
+                task.data['data_store_set'](type='study',
+                                            key=key,
+                                            value=task.data[template_variable])
