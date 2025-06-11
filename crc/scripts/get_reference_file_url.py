7b3184594e124a37015ff401e6a2a7f2ed0aca73
mike cullerton <webstuff@bakednotfried.com>
2025-06-11 07:59:53 -0400

    allow download of reference docs

---
+++ crc/scripts/get_reference_file_url.py
@@ -0,0 +1,34 @@
+import urllib
+
+from crc.api.common import ApiError
+from crc.scripts.script import Script
+from crc.services.reference_file_service import ReferenceFileService
+import flask
+from crc import app
+
+
+class ReferenceFileUrl(Script):
+
+    def get_description(self):
+        return """This is my description"""
+
+    def do_task_validate_only(self, task, study_id, workflow_id, *args, **kwargs):
+        return self.do_task(task, study_id, workflow_id, *args, **kwargs)
+
+    def do_task(self, task, study_id, workflow_id, *args, **kwargs):
+        token = 'not_available'
+        if len(args) > 0:
+            server_name = app.config.get('SERVER_NAME')
+            reference_file_name = args[0]
+            file_url = f"{server_name}/v1.0/reference_file/{reference_file_name}/data"
+            if hasattr(flask.g, 'user'):
+                token = flask.g.user.encode_auth_token()
+            # url = file_url + '?auth_token=' + urllib.parse.quote_plus(token)
+            if ('DEVELOPMENT' in app.config and app.config['DEVELOPMENT'] is True):
+                prefix = 'http://'
+            else:
+                prefix = 'https://'
+            url = f"{prefix}{file_url}?auth_token={urllib.parse.quote_plus(token)}"
+            return url
+        else:
+            raise ApiError('missing_parameter', 'Please specify a reference file name.')
