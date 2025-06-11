4f485036b8fda57c204af5b015bc1fa07262d1f4
mike cullerton <webstuff@bakednotfried.com>
2025-06-11 08:19:07 -0400

    allow download of reference docs

--- crc/scripts/get_reference_file_url.py
+++ crc/scripts/get_reference_file_url.py
@@ -19,8 +19,9 @@ class ReferenceFileUrl(Script):
         token = 'not_available'
         if len(args) > 0:
             server_name = app.config.get('SERVER_NAME')
+            application_root = app.config.get('APPLICATION_ROOT', '/')
             reference_file_name = args[0]
-            file_url = f"{server_name}/v1.0/reference_file/{reference_file_name}/data"
+            file_url = f"{server_name}{application_root}v1.0/reference_file/{reference_file_name}/data"
             if hasattr(flask.g, 'user'):
                 token = flask.g.user.encode_auth_token()
             # url = file_url + '?auth_token=' + urllib.parse.quote_plus(token)
